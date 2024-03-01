633426c667d34fa52e04ce1c1f1779fd0ac0741a
Derangula Vijay <derangulavijay2011@gmail.com>
2024-03-01 14:52:00 +0530

    String Occurances Ordered 2 method

--- src/main/java/com/interviewquestionsanswers/strings/NoOfOccurancesAlphabeticalStrings.java
+++ src/main/java/com/interviewquestionsanswers/strings/NoOfOccurancesAlphabeticalStrings.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -35,4 +36,14 @@ List<String> listofComments = new ArrayList<>();
         .map(e -> Map.entry(e.getKey(), listofComments.stream().filter(e.getValue()).count()))
         .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
         .forEachOrdered(e -> System.out.printf("%-7s%3d%n", e.getKey(), e.getValue()));
+	
+	System.out.println("====================");
+	
+	Function<String, Map.Entry<String, Long>> function = f -> Map.entry(f, listofComments.stream()
+	        .filter(e -> e.toLowerCase().contains(f.toLowerCase())).count());
+	elements.stream().map(function)
+    .sorted(Comparator.comparing(Map.Entry<String, Long>::getValue)
+    .reversed().thenComparing(Map.Entry::getKey))
+    .forEach(System.out::println);
+	
 	}}
