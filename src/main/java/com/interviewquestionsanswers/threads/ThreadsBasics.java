1c525d5eca6a2600512ad23f1c945946c58d94f5
Derangula Vijay <derangulavijay2011@gmail.com>
2024-03-03 15:36:58 +0530

    Threads practice with start() 03/03/2024

--- src/main/java/com/interviewquestionsanswers/threads/ThreadsBasics.java
+++ src/main/java/com/interviewquestionsanswers/threads/ThreadsBasics.java
@@ -1,13 +1,23 @@
 package com.interviewquestionsanswers.threads;
 
+
+class Task1 extends Thread {
+	
+	@Override
+	public void run() {
+		
+	System.out.println("\nTask1 Started");
+	for (int i = 191; i < 199; i++) 
+		System.out.print(" "+i);
+	System.out.println("\nTask1 Done");
+	}
+}
 public class ThreadsBasics {
 
 	public static void main(String[] args) {
 
-
-		for (int i = 191; i < 199; i++) 
-			System.out.print(" "+i);
-		System.out.println("\nTask1 Done");
+		Task1 task1 = new Task1();
+		task1.start();//task1.run();
 		
 		for (int j = 291; j < 299; j++) 
 			System.out.print(" "+j);
