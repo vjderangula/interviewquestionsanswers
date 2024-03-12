f4313175015fb9e9d216a8e2c9bd6f10bf7cbea2
Derangula Vijay <derangulavijay2011@gmail.com>
2024-03-12 15:31:31 +0530

    Thrre Digit Sum [-1,-1, 2] [1,-1,0]

---
+++ src/main/java/com/interviewquestionsanswers/numbers/ThreeSum.java
@@ -0,0 +1,56 @@
+package com.interviewquestionsanswers.numbers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = threeSum(nums);
+        System.out.println("Output: " + result);
+    }
+    
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Arrays.sort(nums); // Step 1: Sort the input array
+        
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = n - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicate elements
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
